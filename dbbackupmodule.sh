3f824c1b434474c782415cdafab92a6967b9c576
aliefamzari <aliefamzari@gmail.com>
2019-12-30 13:25:28 +0800

    add directory check condition to script block

--- dbbackupmodule.sh
+++ dbbackupmodule.sh
@@ -10,8 +10,7 @@
 ##		Version Control: Git			 
 ##################################################
 
-# Backing up mysql or mariadb 
-
+# Backing up mysql or mariadb
 if [ ${databaseType} = "mariadb" ] || [ ${databaseType} = "mysql" ]; then
 	echo "${currentTime} ${infoStrDb} Database type is $databaseType" >> $logPath/ncbackup.log
 		if [ ! -x "$(command -v mysqldump)" ]; then
@@ -22,12 +21,22 @@ if [ ${databaseType} = "mariadb" ] || [ ${databaseType} = "mysql" ]; then
 			echo "${currentTime} ${infoStrDb} See $logPath/ncbackup.log for more details"
 			exit 1
 			else
-				echo "${currentTime} ${infoStrDb} Backing up Database named $dbName to this directory $backupDbDir" >> $logPath/ncbackup.log
-				mysqldump --single-transaction -h localhost -u $dbUserName -p$dbPasswd $dbName > ${backupDbDir}/${fileNameDb}_${currentDate}.sql
-				echo "${currentTime} ${infoStrDb} Backup ${fileNameDb}_${currentDate}.sql created." >> $logPath/ncbackup.log
+				if [ -w ${backupDbDir} ]; then
+					echo "${currentTime} ${infoStrDb} Backing up Database named ${dbName} to this directory ${backupDbDir}" >> $logPath/ncbackup.log
+					mysqldump --single-transaction -h localhost -u ${dbUserName} -p${dbPasswd} ${dbName} > ${backupDbDir}/${fileNameDb}_${currentDate}.sql
+					echo "${currentTime} ${infoStrDb} Backup ${fileNameDb}_${currentDate}.sql created." >> $logPath/ncbackup.log
+					else
+						echo "${currentTime} ${errorStrdb} No write permission to destination directory" | tee -a $logPath/ncbackup.log
+						echo "${currentTime} ${infoStrDb} Restoring main services.." | tee -a $logPath/ncbackup.log
+						DisableMaintenanceMode
+						StartwebSvcUnit
+						echo "${currentTime} ${infoStrDb} See $logPath/ncbackup.log for more details"
+						exit 1
+				fi
 		fi
-	
-	elif [ $databaseType = "postgresql" ]; then
+
+# Backing up postgresql
+	elif [ ${databaseType} = "postgresql" ]; then
 		echo "${currentTime} ${infoStrDb} Database type is $databaseType" >> $logPath/ncbackup.log
 			if [ ! -x $(command -v pg_dump) ]; then
 				echo "Command 'pg_dump' not found. Backup aborted" | tee -a $logPath/ncbackup.log
@@ -37,30 +46,39 @@ if [ ${databaseType} = "mariadb" ] || [ ${databaseType} = "mysql" ]; then
 				echo "${currentTime} ${infoStrDb} See $logPath/ncbackup.log for more details"
 				exit 1
 				else
-					echo "${currentTime} ${infoStrDb} Backing up Database named $dbName to this directory $backupDbDir" >> $logPath/ncbackup.log
-					PGPASSWORD=$dbPasswd pg_dump $dbName -h localhost -U $dbUserName -f ${backupDbDir}/${fileNameDb}_${currentDate}.sql
-					echo "${currentTime} ${infoStrDb} Backup ${fileNameDb}_${currentDate}.sql created." >> $logPath/ncbackup.log
+					if [ -w ${backupDbDir} ]; then
+						echo "${currentTime} ${infoStrDb} Backing up Database named ${dbName} to this directory ${backupDbDir}" >> $logPath/ncbackup.log
+						PGPASSWORD=${dbPasswd} pg_dump ${dbName} -h localhost -U ${dbUserName} -f ${{backupDbDir}}/${fileNameDb}_${currentDate}.sql
+						echo "${currentTime} ${infoStrDb} Backup ${fileNameDb}_${currentDate}.sql created." >> $logPath/ncbackup.log
+						else 
+							echo "${currentTime} ${errorStrdb} No write permission to destination directory" | tee -a $logPath/ncbackup.log
+							echo "${currentTime} ${infoStrDb} Restoring main services.." | tee -a $logPath/ncbackup.log
+							DisableMaintenanceMode
+							StartwebSvcUnit
+							echo "${currentTime} ${infoStrDb} See $logPath/ncbackup.log for more details"
+							exit 1
+					fi
 			fi
 
 fi
 
 # Delete old backup if required
-nrOfDbBackups=$(ls -l ${backupDbDir} | grep -c 'nextcloud-sqlbkp.*sql')
+nrOfDbBackups=$(ls -l ${{backupDbDir}} | grep -c 'nextcloud-sqlbkp.*sql')
 nDbBkToRemove=$(( ${nrOfDbBackups} - ${maxNrOfAppBackups} ))
 
 echo "${currentTime} ${infoStrDb} Checking number of backups available..."  >> $logPath/ncbackup.log
 
 if [ ${maxNrOfDbBackups} != 0 ]; then	
 	echo "${currentTime} ${infoStrDb} Current number of backup available $nrOfDbBackups" >> $logPath/ncbackup.log
-	if [ ${nrOfDbBackups} -gt ${maxNrOfDbBackups} ]; then		
-		echo "${currentTime} ${infoStrDb} Max number of backups is set to $maxNrOfDbBackups. Removing $nDbBkToRemove old backups" >> $logPath/ncbackup.log
-		ls -t ${backupDbDir} | grep 'nextcloud-sqlbkp.*sql' | tail -$nDbBkToRemove |while read -r dbBkToRemove; do
-			rm "${backupDbDir}/${dbBkToRemove}"
-			echo "${currentTime} ${infoStrDb} ${dbBkToRemove} - Remove" >> $logPath/ncbackup.log
-			done
-		else
-			echo "${currentTime} ${infoStrDb} Max number of backups is set to ${maxNrOfDbBackups} to keep. 0 backup removed" >> $logPath/ncbackup.log
-	fi
+		if [ ${nrOfDbBackups} -gt ${maxNrOfDbBackups} ]; then		
+			echo "${currentTime} ${infoStrDb} Max number of backups is set to $maxNrOfDbBackups. Removing $nDbBkToRemove old backups" >> $logPath/ncbackup.log
+			ls -t ${{backupDbDir}} | grep 'nextcloud-sqlbkp.*sql' | tail -$nDbBkToRemove |while read -r dbBkToRemove; do
+				rm "${{backupDbDir}}/${dbBkToRemove}"
+				echo "${currentTime} ${infoStrDb} ${dbBkToRemove} - Remove" >> $logPath/ncbackup.log
+				done
+			else
+				echo "${currentTime} ${infoStrDb} Max number of backups is set to ${maxNrOfDbBackups} to keep. 0 backup removed" >> $logPath/ncbackup.log
+		fi
 	elif [ ${maxNrOfDbBackups} = 0]; then
 		echo "${currentTime} ${infoStrDb} Current no of backups available ${nrOfDbBackups}" >> $logPath/ncbackup.log
 		echo "${currentTime} ${infoStrDb} Max number of backups is set to \"Unlimited\". 0 backup removed" >> $logPath/ncbackup.log
