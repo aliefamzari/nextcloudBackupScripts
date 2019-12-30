bd79c9c6653ba48b12b967be2fb30e790d08acb3
aliefamzari <aliefamzari@gmail.com>
2019-12-30 09:58:01 +0800

    Add dbbackupmodule.sh to remote from DXC Laptpo

---
+++ dbbackupmodule.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+######   Module for mainNcBackup.sh       #####
+## 		Module name: dbbackupmodule.sh			##
+##      Author: Alif Amzari Mohd Azamee         ##
+##		Date: 2019-12-25 				        ##
+##		Job retention: n/a				        ##
+##		Job type: Manual				        ##
+##		Version: 0.1					        ##
+##		Version Control: notGit			        ##
+##################################################
+
+# Backing up mysql or mariadb 
+
+if [ ${databaseType} = "mariadb" ] || [ ${databaseType} = "mysql" ]; then
+	echo ""
+		if [ ! -x "$(command -v mysqldump)" ]; then
+			echo "${currentTime} ${errorStrdb} Command 'mysqldump' not found. Backup aborted" | tee -a $logPath/ncbackup.log
+			echo "${currentTime} ${infoStrDb} Restoring main services.." | tee -a $logPath/ncbackup.log
+			DisableMaintenanceMode
+			StartwebSvcUnit
+			echo "${currentTime} ${infoStrDb} See $logPath/ncbackup.log for more details"
+			exit 1
+			else
+				echo "${currentTime} ${infoStrDb} Backing up Database named $dbName to this directory $backupDbDir" >> $logPath/ncbackup.log
+				mysqldump --single-transaction -h localhost -u $dbUserName -p$dbPasswd $dbName > ${backupDbDir}/${fileNameDb}_${currentDate}.sql
+				echo "${currentTime} ${infoStrDb} Backup ${fileNameDb}_${currentDate}.sql created." >> $logPath/ncbackup.log
+		fi
+	# TODO:	
+	elif [ $databaseType = "postgresql" ]; then
+		echo ""
+
+fi
+
+# Delete old backup if required
+nrOfDbBackups=$(ls -l ${backupDbDir} | grep -c 'nextcloud-sqlbkp.*sql')
+nDbBkToRemove=$(( ${nrOfDbBackups} - ${maxNrOfAppBackups} ))
+
+echo "${currentTime} ${infoStrDb} Checking number of backups available..."  >> $logPath/ncbackup.log
+
+if [ ${maxNrOfDbBackups} != 0 ]; then	
+	echo "${currentTime} ${infoStrDb} Current number of backup available $nrOfDbBackups" >> $logPath/ncbackup.log
+	if [ ${nrOfDbBackups} -gt ${maxNrOfDbBackups} ]; then		
+		echo "${currentTime} ${infoStrDb} Max number of backups is set to $maxNrOfDbBackups. Removing $nDbBkToRemove old backups" >> $logPath/ncbackup.log
+		ls -t ${backupDbDir} | grep 'nextcloud-sqlbkp.*sql' | tail -$nDbBkToRemove |while read -r dbBkToRemove; do
+			rm "${backupDbDir}/${dbBkToRemove}"
+			echo "${currentTime} ${infoStrDb} ${dbBkToRemove} - Remove" >> $logPath/ncbackup.log
+			done
+		else
+			echo "${currentTime} ${infoStrDb} Max number of backups is set to ${maxNrOfDbBackups} to keep. 0 backup removed" >> $logPath/ncbackup.log
+	fi
+	elif [ ${maxNrOfDbBackups} = 0]; then
+		echo "${currentTime} ${infoStrDb} Current no of backups available ${nrOfDbBackups}" >> $logPath/ncbackup.log
+		echo "${currentTime} ${infoStrDb} Max number of backups is set to \"Unlimited\". 0 backup removed" >> $logPath/ncbackup.log
+fi
+echo "${currentTime} ${infoStrDb} Nextcloud Database backup completed" >> $logPath/ncbackup.log
+
